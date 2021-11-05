f8436d51b6c1804df40b202fb0fd7b8166d0bcc8
Fabien LOISON <flo@flogisoft.com>
2021-11-05 11:40:02 +0100

    Move fileListToModule to helpers.

--- app/sources/browserify.js
+++ app/sources/browserify.js
@@ -34,30 +34,9 @@ function _listBundledFiles(entryPointsPath, ignore) {
     });
 }
 
-function _fileListToModule(files) {
-    var modules = [];
-    var modulePaths = {};
-    for (let i = 0 ; i < files.length ; i++) {
-        if (files[i].indexOf("node_modules") < 0) {
-            continue;
-        }
-        let file = files[i];
-        let module = helpers.newModule({
-            name: file.replace(/^(.*node_modules[/\\]((@[^/\\]+[/\\])?[^/\\]+)[/\\]).*$/, "$2"),
-            path: file.replace(/^(.*node_modules[/\\]((@[^/\\]+[/\\])?[^/\\]+)[/\\]).*$/, "$1"),
-        });
-        module.website = `https://www.npmjs.com/package/${module.name}`;
-        if (!modulePaths[module.path]) {
-            modulePaths[module.path] = true;
-            modules.push(module);
-        }
-    }
-    return modules;
-}
-
 function sourceBrowserify(entryPointsPath, ignore) {
     return _listBundledFiles(entryPointsPath, ignore)
-        .then(_fileListToModule)
+        .then(helpers.fileListToModule)
         .then(extractors.nodeModule);
 }
 
